77e441cf5ca979f5d5233696df2103edb5629532
Luna-Scott <est.luna.scott@unimilitar.edu.co>
2024-08-27 15:33:16 -0500

    Create pg2.java

---
+++ P1/pg2.java
@@ -0,0 +1,18 @@
+/*Apartir de la clase triangulo crea sobrecarga al contructor crear
+dos objetos para cada una de las formas del constructor*/
+
+public class Triangulo {
+    public void base() {
+}
+    public void altura() {
+}
+
+public class Forma extends Triangulo {
+    public void base() {
+        System.out.println("");
+    }
+
+    public void altura() {
+        System.out.println("");
+    }
+}
