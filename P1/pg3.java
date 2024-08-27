8c0a1a436b52a2fab5bd6eab554ce0ecdb4fa5d6
Luna-Scott <est.luna.scott@unimilitar.edu.co>
2024-08-27 15:44:21 -0500

    Create pg3.java

---
+++ P1/pg3.java
@@ -0,0 +1,18 @@
+/*Crear dos metodos puros que permitan obtener area y perimetro 
+del triangulo asumiendo que todos son triangulos rectangulos*/
+
+public class Triangulo {
+        
+    public static int main(String[] base, altura) {
+        int base = x;
+        int altura = y;
+      }
+       
+    public static int area(int x, int y) {
+        return x * y/2;
+    }
+
+    public static int perimetro(int x, int y) {
+        return x + y ;
+    }
+}
