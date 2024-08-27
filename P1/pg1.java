e4446146a78fa11fdbf0252de0d5f62cdd77b1f6
Luna-Scott <est.luna.scott@unimilitar.edu.co>
2024-08-27 15:23:10 -0500

    Create pg1.java

---
+++ P1/pg1.java
@@ -0,0 +1,10 @@
+/*Crear una clase llamada triangulo con atributos base y altura, 
+ aplicar encapsulamiento y los metodos apropiados para 
+ inicializar o crear objetos con sus respectivos atributos*/
+
+    public class Triangulo {
+        public void base() {
+        }
+        public void altura(){ 
+        }
+    }
